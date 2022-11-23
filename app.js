51efc4cb60064610018987db153e1c3515789bd4
Valentin Himself <valentinhimself@gmail.com>
2022-11-23 10:24:23 -0700

    Add button animations

--- app.js
+++ app.js
@@ -9,4 +9,16 @@ Book.prototype.output = function(){
     return `${this.name} by ${this.author} is ${this.pages} pages long. Has it been read? ${this.read}`
 }
 
-//const theHobbit = new Book('The Hobbit', 'J.R.R. Tolkien', '295', 'Yes');
+const form = document.querySelector('form');
+const addBookBtn = document.querySelector('.add__div button');
+const tint = document.querySelector('.tint')
+addBookBtn.addEventListener('click', expandForm);
+
+function expandForm() {
+    form.classList.add('scale');
+    applyTint();
+}
+
+function applyTint() {
+    tint.classList.add('opacity');
+}
